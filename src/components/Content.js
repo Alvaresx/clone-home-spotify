ca26be004619100bfb7a5cdd1fcfc41bfc7b5956
Mariana Alvares <mariana11areal@hotmail.com>
2022-04-05 21:33:04 -0300

    Content parcialmente concluído

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Content = () => {
-  return <div className="bg-[#2941ab] h-[846px]">Content</div>;
+  return (
+    <div className="bg-[#2941ab] h-[846px]">
+      <h1 className="text-[#1ed760] font-bold text-[150px] tracking-tight leading-[150px] flex justify-center text-center pt-40">
+        Escutar muda <br />
+        tudo
+      </h1>
+      <h6 className="text-center mt-20 text-[#1ed760] font-semibold text-lg">
+        Milhões de músicas e podcasts para explorar. E nem precisa de cartão de
+        crédito.
+      </h6>
+      <button className="flex mx-auto my-0 mt-10 text-[#2941ab] font-bold bg-[#1ed760] py-3 px-8 uppercase rounded-full">Baixe o Spotify Free</button>
+    </div>
+  );
 };
 
 export default Content;
